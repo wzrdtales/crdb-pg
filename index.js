bc0fb796fb4940a24753914f6456ae74e514832e
Tobias Gurtzick <magic@wizardtales.com>
2018-02-20 20:08:01 +0100

    create working retry
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 'use strict';
 
 let pg = require('pg');
+const SQL = require('sql-template-strings');
 
-function retry (savepoint = 'cockroach_restart') {
-  this.query(`BEGIN; SAVEPOINT ${savepoint}`);
-
-  let query = (...args) => {
-    const cb = args[args.length - 1];
-
-    if (typeof cb === 'function') {
-      const retry = (err, res) => {
-        if (err && err.code === '40001') {
-          return this.client.query(`ROLLBACK TO SAVEPOINT ${savepoint}`, retry);
-        }
-
-        return cb(err, res);
-      };
-      args[args.length - 1] = retry;
-
-      // call through
-      this.query.apply(this, args);
-    } else {
-      return this.query.apply(this, args).catch(err => {
-        if (err.code === '40001') {
-          return this.client.query(`ROLLBACK TO SAVEPOINT ${savepoint}`);
-        }
-
-        return Promise.reject(err);
-      });
+async function retry (pool, callQueries) {
+  const client = await this.connect();
+  await client.query(SQL`BEGIN; SAVEPOINT cockroach_restart`);
+  async function handleError (err) {
+    if (err.code === '40001') {
+      await client.query(SQL`ROLLBACK TO SAVEPOINT cockroach_restart`);
+      return exec();
     }
-  };
 
-  return { query };
+    throw err;
+  }
+  async function exec () {
+    try {
+      const result = await callQueries(client);
+      await client.query(SQL`RELEASE SAVEPOINT cockroach_restart`);
+      return result;
+    } catch (err) {
+      return handleError(err);
+    }
+  }
+
+  return exec();
 }
 
 module.exports = class CRDB {
@@ -54,22 +47,8 @@ module.exports = class CRDB {
   pool () {
     this._discover();
     let pool = new pg.Pool(this._config);
-    pool.retry = retry;
-    pool._realConnect = pool.connect;
+    pool.retry = retry; // patch in retry
 
-    pool.connect = function (...args) {
-      if (typeof args[0] === 'function') {
-        pool._realConnect((err, client, release) => {
-          client.retry = retry;
-          args[0](err, client, release);
-        });
-      } else {
-        return pool._realConnect.apply(pool, args).then(client => {
-          client.retry = retry;
-          return client;
-        });
-      }
-    };
     return pool;
   }
 
